5b37812d4cac0cfe69f1dbd1c0022b7b9de7633e
rul-duran <raulduran816@gmail.com>
2023-10-21 23:46:36 -0600

    feat: setup devtools

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,42 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2020: true,
+    "jest/globals": true
+  },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'plugin:jest/recommended',
+    'plugin:jest/style',
+    'airbnb',
+    'prettier'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: [
+    'react-refresh',
+    'prettier',
+    'jest'
+  ],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'react/function-component-definition': [
+      2,
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+    'max-len': ['error', {'code': 80, 'ignoreUrls': true}],
+    'eol-last': ['error', 'always'],
+    'prettier/prettier': ['error']
   },
 }
